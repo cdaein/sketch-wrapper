fb648d04a43fc6aece0990ba4ffac0abf2c0e56e
user <3522003+cdaein@users.noreply.github.com>
2023-01-14 15:15:41 -0500

    add try/catch to ogl import

--- dist/index.js
+++ dist/index.js
@@ -23,44 +23,48 @@ var init_ogl = __esm({
       let [width, height] = settings.dimensions;
       let pixelRatio = Math.max(settings.pixelRatio, 1);
       const attributes = settings.attributes;
-      const Renderer = (await import('ogl-typescript')).Renderer;
-      const renderer = new Renderer({
-        width,
-        height,
-        dpr: settings.pixelRatio,
-        ...attributes
-      });
-      const gl = renderer.gl;
-      let canvas = gl.canvas;
-      ({ canvas, width, height, pixelRatio } = setupCanvas({
-        parent: settings.parent,
-        canvas,
-        width,
-        height,
-        pixelRatio
-      }));
-      if (settings.centered === true) {
-        const canvasContainer = canvas.parentElement;
-        canvasContainer.style.width = "100vw";
-        canvasContainer.style.height = "100vh";
-        canvasContainer.style.display = "flex";
-        canvasContainer.style.justifyContent = "center";
-        canvasContainer.style.alignItems = "center";
-        if (settings.scaleContext === false) ;
-      } else {
-        canvas.style.width = 100 + "%";
-        canvas.style.height = 100 + "%";
-        canvas.style.maxWidth = `${settings.dimensions[0]}px`;
-        canvas.style.maxHeight = `${settings.dimensions[1]}px`;
+      try {
+        const Renderer = (await import('ogl-typescript')).Renderer;
+        const renderer = new Renderer({
+          width,
+          height,
+          dpr: settings.pixelRatio,
+          ...attributes
+        });
+        const gl = renderer.gl;
+        let canvas = gl.canvas;
+        ({ canvas, width, height, pixelRatio } = setupCanvas({
+          parent: settings.parent,
+          canvas,
+          width,
+          height,
+          pixelRatio
+        }));
+        if (settings.centered === true) {
+          const canvasContainer = canvas.parentElement;
+          canvasContainer.style.width = "100vw";
+          canvasContainer.style.height = "100vh";
+          canvasContainer.style.display = "flex";
+          canvasContainer.style.justifyContent = "center";
+          canvasContainer.style.alignItems = "center";
+          if (settings.scaleContext === false) {
+          }
+        } else {
+          canvas.style.width = 100 + "%";
+          canvas.style.height = 100 + "%";
+          canvas.style.maxWidth = `${settings.dimensions[0]}px`;
+          canvas.style.maxHeight = `${settings.dimensions[1]}px`;
+        }
+        return {
+          canvas,
+          oglContext: gl,
+          oglRenderer: renderer,
+          width,
+          height,
+          pixelRatio
+        };
+      } catch (e) {
       }
-      return {
-        canvas,
-        oglContext: gl,
-        oglRenderer: renderer,
-        width,
-        height,
-        pixelRatio
-      };
     };
   }
 });
--- src/modes/ogl.ts
+++ src/modes/ogl.ts
@@ -8,56 +8,60 @@ export const createOglCanvas = async (settings: SketchSettingsInternal) => {
 
   const attributes = settings.attributes as WebGLContextAttributes;
 
-  const Renderer = (await import("ogl-typescript")).Renderer;
-  const renderer = new Renderer({
-    width,
-    height,
-    dpr: settings.pixelRatio,
-    // TODO: add attributes {} to settings
-    //       enalbes image export with OGLContext. but it can make things slower
-    // preserveDrawingBuffer: true,
-    ...attributes,
-  });
-  const gl = renderer.gl;
-  let canvas = gl.canvas;
+  try {
+    const Renderer = (await import("ogl-typescript")).Renderer;
+    const renderer = new Renderer({
+      width,
+      height,
+      dpr: settings.pixelRatio,
+      // TODO: add attributes {} to settings
+      //       enalbes image export with OGLContext. but it can make things slower
+      // preserveDrawingBuffer: true,
+      ...attributes,
+    });
+    const gl = renderer.gl;
+    let canvas = gl.canvas;
 
-  ({ canvas, width, height, pixelRatio } = setupCanvas({
-    parent: settings.parent,
-    canvas,
-    width,
-    height,
-    pixelRatio,
-  }));
+    ({ canvas, width, height, pixelRatio } = setupCanvas({
+      parent: settings.parent,
+      canvas,
+      width,
+      height,
+      pixelRatio,
+    }));
 
-  // canvas centering
-  // TODO: needs extra scaling of style.width & height to fit window/container
-  // REVIEW: this is probably be better done in index.html <style>
-  //         but, for now, this module doesn't provide html file, so...
-  if (settings.centered === true) {
-    const canvasContainer = canvas.parentElement!;
-    canvasContainer.style.width = "100vw";
-    canvasContainer.style.height = "100vh";
-    canvasContainer.style.display = "flex";
-    canvasContainer.style.justifyContent = "center";
-    canvasContainer.style.alignItems = "center";
+    // canvas centering
+    // TODO: needs extra scaling of style.width & height to fit window/container
+    // REVIEW: this is probably be better done in index.html <style>
+    //         but, for now, this module doesn't provide html file, so...
+    if (settings.centered === true) {
+      const canvasContainer = canvas.parentElement!;
+      canvasContainer.style.width = "100vw";
+      canvasContainer.style.height = "100vh";
+      canvasContainer.style.display = "flex";
+      canvasContainer.style.justifyContent = "center";
+      canvasContainer.style.alignItems = "center";
 
-    if (settings.scaleContext === false) {
-      // TODO: centering does not work at pixelRatio=2
+      if (settings.scaleContext === false) {
+        // TODO: centering does not work at pixelRatio=2
+      }
+    } else {
+      // scale canvas even when not centered.
+      canvas.style.width = 100 + "%";
+      canvas.style.height = 100 + "%";
+      canvas.style.maxWidth = `${settings.dimensions[0]}px`;
+      canvas.style.maxHeight = `${settings.dimensions[1]}px`;
     }
-  } else {
-    // scale canvas even when not centered.
-    canvas.style.width = 100 + "%";
-    canvas.style.height = 100 + "%";
-    canvas.style.maxWidth = `${settings.dimensions[0]}px`;
-    canvas.style.maxHeight = `${settings.dimensions[1]}px`;
-  }
 
-  return {
-    canvas,
-    oglContext: gl,
-    oglRenderer: renderer,
-    width,
-    height,
-    pixelRatio,
-  };
+    return {
+      canvas,
+      oglContext: gl,
+      oglRenderer: renderer,
+      width,
+      height,
+      pixelRatio,
+    };
+  } catch (e) {
+    //
+  }
 };
